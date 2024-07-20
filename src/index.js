24ef320ab6e74284a23bdfe63b3622f627f08ef1
GianfrancoD <gianfranco.dfiore@gmail.com>
2024-07-20 18:46:53 -0400

    add modifications index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import useHttpRequest from "./useHttpRequest";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
 export { useHttpRequest };
